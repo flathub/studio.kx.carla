f50a9f36594ffdfa1facc661341a8a2238cd7a5f
Bleuzen <12885163+Bleuzen@users.noreply.github.com>
2022-09-11 02:46:38 +0200

    fix vst path in wrapper script

--- carla-wrap.sh
+++ carla-wrap.sh
@@ -2,7 +2,7 @@
 
 export LADSPA_PATH="$HOME/.ladspa:/app/extensions/Plugins/ladspa:/app/lib/ladspa"
 export LV2_PATH="$HOME/.lv2:/app/extensions/Plugins/lv2:/app/lib/lv2"
-export VST_PATH="$HOME/.vst:/app/extensions/Plugins/lxvst"
+export VST_PATH="$HOME/.vst:/app/extensions/Plugins/vst"
 export VST3_PATH="$HOME/.vst3:/app/extensions/Plugins/vst3"
 export DSSI_PATH="$HOME/.dssi:/app/extensions/Plugins/dssi"
 
