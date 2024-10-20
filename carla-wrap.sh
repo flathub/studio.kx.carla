e9c8b9ab7405e3b1e4c76133289cf09e48441151
Hubert Figuière <hub@figuiere.net>
2024-10-20 12:07:27 -0400

    Fix wrap argument forwarding
    Signed-off-by: Hubert Figuière <hub@figuiere.net>

--- carla-wrap.sh
+++ carla-wrap.sh
@@ -6,4 +6,4 @@ export VST_PATH="$HOME/.vst:/app/extensions/Plugins/vst"
 export VST3_PATH="$HOME/.vst3:/app/extensions/Plugins/vst3"
 export DSSI_PATH="$HOME/.dssi:/app/extensions/Plugins/dssi"
 
-exec carla.bin $*
+exec carla.bin "$@"
